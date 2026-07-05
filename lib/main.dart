2ccb223d917506397e6128a02d09c8dd66c1e9a8
Sameer khan <skhandeshwali01@gmail.com>
2026-07-05 08:54:56 +0530

    feat: initialize Supabase and setup BLoC provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quote_app_with_supabase/app.dart';
+import 'package:quote_app_with_supabase/core/constants/env.dart';
+import 'package:quote_app_with_supabase/features/quote/data/datasources/quote_remote_datasource_impl.dart';
+import 'package:quote_app_with_supabase/features/quote/data/repositories/quote_repository_impl.dart';
+import 'package:quote_app_with_supabase/features/quote/view_model/home_bloc.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: Env.supabaseUrl,
+    publishableKey: Env.publishableKey,
+  );
+  runApp(
+    BlocProvider(
+      create: (context) =>
+          HomeBloc(QuoteRepositoryImpl(QuotesRemoteDatasourceImpl())),
+      child: const MainApp(),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
-}
